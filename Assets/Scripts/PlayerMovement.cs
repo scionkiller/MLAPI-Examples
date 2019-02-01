9c82306dee01fd305702daf82cbe6e974826b192
scionkiller <cozeroff@gmail.com>
2019-02-01 13:50:36 -0700

    Big first pass at dramatically simplifying the example, deleting all kinds of code we don't need. Also refactored main scene, and renamed to main scene. Also removed packages not being used. Also simplified input and removed namespaces we don't need right now. Moved MLAPI editor and runtime stuff into a single folder, in preparation for Siobhan to bring in entire MLAPI depot. Most functionality still works, just broke player translation somehow.

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,18 @@
+﻿using MLAPI;
+using MLAPI.MonoBehaviours.Core;
+using MLAPI.NetworkingManagerComponents;
+using MLAPI.NetworkingManagerComponents.Core;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : NetworkedBehaviour
+{
+    void Update ()
+    {
+		if(isLocalPlayer)
+        {
+            transform.Translate(new Vector3(Input.GetAxis("Horizontal") * 2f * Time.deltaTime, 0, Input.GetAxis("Vertical") * 2f * Time.deltaTime));
+		}
+	}
+}
